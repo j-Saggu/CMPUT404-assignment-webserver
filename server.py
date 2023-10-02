e05ed051c953183e69cc12daa57370ff2810b5d6
Jaskirat <jaskiratsaggu1@gmail.com>
2023-10-02 15:03:09 -0600

    working code

--- server.py
+++ server.py
@@ -32,6 +32,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
+        self.reroute = False
         print ("\n\nGot a request of: %s" % self.data)
         self.path = self.create_path()
         self.check_path()
@@ -39,9 +40,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
         
     def create_path(self):
         self.link = self.data.decode("utf-8").split(" ")
-        path = './www' + self.link[1]
+        if ("www" in self.link[1]):
+            self.reroute = True
+            path =  "." + self.link[1]
+        else:
+            path = './www' + self.link[1]
+            
         print(path)
-
         return path
     
     def get_content_type(self):
@@ -50,25 +55,47 @@ class MyWebServer(socketserver.BaseRequestHandler):
         elif "html" in self.link[1]:
             return "html\n" 
         else:
-            return "html"   # 404
+            return "html\n"   # 404
     
-    def send_content(self, error, isReroute):
+    def send_content(self, error, isReroute, isDir):
         statusCode = "HTTP/1.1 "
         contentType = "Content-Type: text/"
         location = "Location: " 
+        
+        if self.reroute:
+            error = "301\n"
+            
         msg = statusCode + error + contentType + self.get_content_type()
         
-        if isReroute:
-            msg += location + self.path + "/"
-            print(msg)
-    
+        # print("404" not in error)
+        print(msg)
+        if "404" not in error and "405" not in error:
+            if isReroute or self.reroute:
+                msg += location + self.link[1]
+
+            if isDir:
+                print("here")
+                contents = self.path + "/index.html"
+                with open(contents, "r") as fileObj:
+                    file = fileObj.read()
+                msg += "\n" + file
+            else:
+                print("here")
+                contents = self.path
+                with open(contents, "r") as fileObj:
+                    file = fileObj.read()
+                msg += "\n" + file
+            
+        msg += "\r\n"
+        print(msg)
+        
         self.request.sendall(msg.encode('utf-8'))
         
     def check_path(self):
         if self.link[0] != "GET":           # only allow GET http commands
-            self.send_content("405\n", False)
+            self.send_content("405\n", False, False)
         elif ".." in self.link[1]:          # do not allow for /../... cases at all
-            self.send_content("404\n", False)
+            self.send_content("404\n", False, False)
             
         try:
             open(self.path)
@@ -78,20 +105,27 @@ class MyWebServer(socketserver.BaseRequestHandler):
             if os.path.exists(self.path):
                 if os.path.isdir(self.path):
                     if not self.path.endswith("/"): 
-                        self.send_content("301\n", True)      # if directory found, return 200
-
+                        self.path += "/"
+                        self.reroute = True
+                        self.send_content("301\n", True, True)      # if directory found, return 200
                         print("301 success")
-                print("success")
-                self.send_content("200\n", False)      # if directory found, return 200
-            # elif os.path.exists(self.path + "/") and os.path.isfile(self.path + "/"):
-            #     print("asfdasfdsafads")
-            #     self.send_content("301\n", True)      # if directory found, return 200
-            else:
+                    else:
+                        self.send_content("200\n", False, True)      # if directory found, return 200
+                else:
+                    self.reroute = False
+                    print("success")    
+                    self.send_content("200\n", False, False)      # if directory found, return 200
+            else:                                                   # not a directory
                 print("not found")
-                self.send_content("404\n", False)      # if directory not found, return 404
+                self.reroute = False
+                self.send_content("404\n", False, False)      # if directory not found, return 404
         else:
-            print("retewrtre")
-            self.send_content("200\n", False)      # if directory found, return 200
+            if os.path.isdir(self.path):
+                print("success")
+                self.send_content("200\n", False, True)      # if directory found, return 200
+            else:
+                print("success")
+                self.send_content("200\n", False, False)      # if directory found, return 200
             
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
